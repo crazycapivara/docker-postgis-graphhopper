2216b02a9ebba480fc0deeb111155a6990390e30
skuethe <crazycapivara@gmail.com>
2019-04-02 13:41:27 +0100

    Add function to get graphhopper pointlist

--- python/graphhopper/__init__.py
+++ python/graphhopper/__init__.py
@@ -11,7 +11,22 @@ class Graphhopper(object):
             "time_limit": time_limit,
             "buckets": buckets
         }
-        api_url = requests.compat.urljoin(self.base_url, "isochrone")
+        # api_url = requests.compat.urljoin(self.base_url, "isochrone")
+        # response = requests.get(api_url, params = params)
+        # return response
+        return self.response("isochrone", params)
+
+    def pointlist(self, lat, lng, time_limit):
+        params = {
+            "point": "%s,%s" % (lat, lng),
+            "result": "pointlist",
+            "time_limit": time_limit,
+            "pointlist_ext_header": "prev_longitude,prev_latitude,node_id,prev_node_id"
+        }
+        return self.response("isochrone", params)
+
+    def response(self, path, params):
+        api_url = requests.compat.urljoin(self.base_url, path)
         response = requests.get(api_url, params = params)
         return response
-
+    
