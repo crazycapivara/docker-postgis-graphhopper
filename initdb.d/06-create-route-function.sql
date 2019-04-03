0a8cc7ffa79bed808a3544aed1096c4423bc65bf
skuethe <crazycapivara@gmail.com>
2019-04-03 10:02:31 +0100

    Add route function

---
+++ initdb.d/06-create-route-function.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION graphhopper.route(
+  from_lat double precision,
+  from_lng double precision,
+  to_lat double precision,
+  to_lng double precision)
+
+  RETURNS TABLE(route jsonb, "time" double precision, distance double precision) 
+  LANGUAGE 'plpython3u'
+
+  COST 100
+  VOLATILE
+  ROWS 1000
+AS $BODY$
+
+import os
+import sys
+import json
+
+sys.path.append("/python-libs")
+import graphhopper as gh
+
+base_url = os.environ.get("GH_API_URL") or "http://graphhopper:8989/"
+api_client = gh.Graphhopper(base_url)
+response = api_client.route(from_lat, from_lng, to_lat, to_lng)
+data = response.json()
+plpy.notice(response.url)
+path = data["paths"][0]
+return [ (json.dumps(path["points"]), path["time"], path["distance"]) ]
+
+$BODY$;
+
