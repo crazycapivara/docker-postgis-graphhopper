1fb2e7598f87d9593b9424feaefc96b75af84148
skuethe <crazycapivara@gmail.com>
2019-04-08 08:04:23 +0100

    Refactor

--- examples/route-matrix.sql
+++ examples/route-matrix.sql
@@ -3,16 +3,16 @@ WITH routes AS (
     a.id AS source_id,
     b.id AS target_id,
     (SELECT row_to_json(r) FROM graphhopper.route(ST_Y(a.geom), ST_X(a.geom), ST_Y(b.geom), ST_X(b.geom)) AS r) AS route
-    FROM graphhopper.points_berlin AS a
+  FROM graphhopper.points_berlin AS a
       CROSS JOIN graphhopper.points_berlin AS b
-    WHERE a.id IN (3, 4)
+  WHERE a.id IN (3, 4)
 )
 
 SELECT
   source_id,
   target_id,
   -- ST_SetSRID(ST_geomFromGeoJSON(route->'route'), 4326) AS linestring,
-  cast(route->>'time' as integer) AS "time",
-  cast(route->>'distance' as double precision) AS distance
-  FROM routes
+  cast(route->>'time' AS integer) AS "time",
+  cast(route->>'distance' AS double precision) AS distance
+FROM routes
 
