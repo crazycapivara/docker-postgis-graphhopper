30aefcc341fdaa6ec6d560ff63daa6397bcf1b49
skuethe <crazycapivara@gmail.com>
2019-04-10 10:49:39 +0100

    Fix bug hexagon function

--- initdb.d/09-create-function-h3-hexagons.sql
+++ initdb.d/09-create-function-h3-hexagons.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE FUNCTION graphhopper.h3_hexagons (
   lat double precision,
   lng double precision,
-  resolution integer
+  time_limit integer DEFAULT 200,
+  resolution integer DEFAULT 9
 )
 RETURNS TABLE (
   h3_index h3index,
@@ -14,10 +15,10 @@ AS $$
     RETURN QUERY
     WITH base AS (
       SELECT
-        h3_geo_to_h3(ST_MakePoint(h.lng, h.lat), 9) AS h3_index,
+        h3_geo_to_h3(ST_MakePoint(h.lng, h.lat), resolution) AS h3_index,
         avg(h."time") AS "time",
         avg(h.distance) AS distance
-      FROM graphhopper.pointlist(lat, lng) AS h GROUP BY h3_index
+      FROM graphhopper.pointlist(lat, lng, time_limit) AS h GROUP BY h3_index
     )
     SELECT b.h3_index, b."time", b.distance, ST_SetSRID(h3_to_geo_boundary_geometry(b.h3_index), 4326) AS geom FROM base AS b;
   END
